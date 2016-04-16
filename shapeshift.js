d5159038e5aea1732b4bcbae8972be5df8398b98
Killscreen <killscreen@users.noreply.github.com>
2016-04-15 19:15:57 -0700

    Move the ball around

--- shapeshift.js
+++ shapeshift.js
@@ -1,3 +1,52 @@
+function Loop(schedule) {
+  this.schedule = schedule;
+  this.active = false;
+}
+
+Loop.prototype.start = function () {
+  if (this.active) return;
+  this.active = true;
+  this.next();
+};
+
+Loop.prototype.next = function () {
+  if (!this.active) return;
+  this.body();
+  this.schedule(this.next.bind(this));
+};
+
+Loop.prototype.stop = function () {
+  this.active = false;
+}
+
+
+function RenderingLoop(renderer, scene, camera) {
+  Loop.call(this, window.requestAnimationFrame.bind(window));
+  this.renderer = renderer;
+  this.scene = scene;
+  this.camera = camera;
+}
+
+RenderingLoop.prototype = Object.create(Loop.prototype);
+
+RenderingLoop.prototype.body = function () {
+  this.renderer.render(this.scene, this.camera);
+};
+
+
+function PhysicsLoop(mesh) {
+  Loop.call(this, function (callback) {
+    window.setTimeout(callback, 15);
+  });
+  this.mesh = mesh;
+}
+
+PhysicsLoop.prototype = Object.create(Loop.prototype);
+
+PhysicsLoop.prototype.body = function () {
+  this.mesh.position.x = Math.sin(Date.now() / 1000);
+};
+
 function initialize() {
   var canvas = document.getElementById('main');
   var w = canvas.width, h = canvas.height;
@@ -20,7 +69,9 @@ function initialize() {
   scene.add(light);
 
   renderer.setSize(canvas.width, canvas.height);
-  renderer.render(scene, camera);
+
+  new RenderingLoop(renderer, scene, camera).start();
+  new PhysicsLoop(mesh).start();
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
