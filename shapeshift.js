b5728bb508e697d51c5e9883c0b2261472e05703
Killscreen <killscreen@users.noreply.github.com>
2016-04-15 18:45:24 -0700

    start using three

--- shapeshift.js
+++ shapeshift.js
@@ -1,5 +1,12 @@
 function initialize() {
-  window.alert("SHAPESHIFT!");
+  var canvas = document.getElementById('main');
+  var w = canvas.width, h = canvas.height;
+  var renderer = new THREE.WebGLRenderer({ canvas: canvas });
+  var camera = new THREE.PerspectiveCamera(45, w / h, 0.01, 100000);
+  var scene = new THREE.Scene();
+
+  scene.add(camera);
+  renderer.setSize(canvas.width, canvas.height);
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
