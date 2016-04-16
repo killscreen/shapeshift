dd78d257eec48343b982f735264e03c625d3e8fe
Killscreen <killscreen@users.noreply.github.com>
2016-04-15 18:55:41 -0700

    show sphere

--- shapeshift.js
+++ shapeshift.js
@@ -5,8 +5,22 @@ function initialize() {
   var camera = new THREE.PerspectiveCamera(45, w / h, 0.01, 100000);
   var scene = new THREE.Scene();
 
+  var material = new THREE.MeshLambertMaterial({ color: 0x0000DD });
+  var geometry = new THREE.SphereGeometry(2, 12, 12);
+  var mesh = new THREE.Mesh(geometry, material);
+  var light = new THREE.PointLight(0xFFFFFF);
+
+  light.position.x = 5;
+  light.position.y = 5;
+
+  mesh.position.z = -10;
+
   scene.add(camera);
+  scene.add(mesh);
+  scene.add(light);
+
   renderer.setSize(canvas.width, canvas.height);
+  renderer.render(scene, camera);
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
